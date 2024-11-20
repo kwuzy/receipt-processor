77b6c4b7bbc0fd0261c8a7775772579748b39348
kwuzy <30635580+kwuzy@users.noreply.github.com>
2024-11-20 15:16:24 -0500

    date and time based points

--- services/receiptsServices.go
+++ services/receiptsServices.go
@@ -22,14 +22,25 @@ func GetReceiptPoints(id string) (int, error) {
 		return 0, errors.New("Receipt not found")
 	}
 
-	points := 0
-	// One point for every alphanumeric character in the retailer name.
-	points += countAlphanumeric(receipt.Retailer)
-	// 50 points if the total is a round dollar amount with no cents.
+	// Data Conversions
 	totalPrice, err := convertStringToPrice(receipt.Total)
 	if err != nil {
 		return 0, err
 	}
+	day, err := getDayFromDate(receipt.PurchaseDate)
+	if err != nil {
+		return 0, err
+	}
+	isBetweenTwoAndFour, err := isBetweenTwoAndFour(receipt.PurchaseTime)
+	if err != nil {
+		return 0, err
+	}
+
+	// Points Calculations
+	points := 0
+	// One point for every alphanumeric character in the retailer name.
+	points += countAlphanumeric(receipt.Retailer)
+	// 50 points if the total is a round dollar amount with no cents.
 	if totalPrice == float32(int(totalPrice)) {
 		points += 50
 	}
@@ -40,8 +51,15 @@ func GetReceiptPoints(id string) (int, error) {
 	// 5 points for every two items on the receipt.
 	points += 5 * int(len(receipt.Items)/2)
 	// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+
 	// 6 points if the day in the purchase date is odd.
+	if day%2 == 1 {
+		points += 6
+	}
 	// 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+	if isBetweenTwoAndFour {
+		points += 10
+	}
 
 	return points, nil
 }
--- services/utils.go
+++ services/utils.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math"
 	"strconv"
+	"time"
 	"unicode"
 )
 
@@ -26,4 +27,27 @@ func convertStringToPrice(s string) (float32, error) {
 	price = math.Floor(price*100) / 100
 
 	return float32(price), nil
+}
+
+func getDayFromDate(s string) (int, error) {
+	layout := "2006-01-02"
+	t, err := time.Parse(layout, s)
+	if err != nil {
+		return 0, errors.New("Date format is invalid")
+	}
+
+	return t.Day(), nil
+}
+
+func isBetweenTwoAndFour(s string) (bool, error) {
+	layout := "15:04"
+	t, err := time.Parse(layout, s)
+	if err != nil {
+		return false, errors.New("Time format is invalid")
+	}
+
+	startTime, _ := time.Parse(layout, "14:00")
+	endTime, _ := time.Parse(layout, "16:00")
+
+	return t.After(startTime) && t.Before(endTime), nil
 }
